89592eba55f32743465dff72c343b8efd8dff849
Dilshodbek Matyaqubov <DilshodbekNodejs@gmail.com>
2024-04-05 12:15:01 +0500

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Server ${PORT} da ishga tushdi `))
